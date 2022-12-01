03b96f0db91ac9df031d2009ea375ee1aed08ad2
elizeulins <elizeulins@hotmail.com>
2022-11-30 21:00:49 -0300

    teste

---
+++ scripts.js
@@ -0,0 +1,62 @@
+function verificar1(){
+  var resposta = document.getElementById("certa1");
+  
+  if(resposta.checked){
+    alert("Acertou. Parabéns!");
+  }
+  else{
+    alert("Errou. que pena!");
+  }
+}
+
+function verificar2(){
+  var resposta = document.getElementById("certa2");
+  
+  if(resposta.checked){
+    alert("Acertou. Parabéns!");
+  }
+  else{
+    alert("Errou. que pena!");
+  }
+
+}
+
+function verificar3(){
+  var resposta = document.getElementById("certa3");
+  
+  if(resposta.checked){
+    alert("Acertou. Parabéns!");
+  }
+  else{
+    alert("Errou. que pena!");
+  }
+
+}
+
+function verificar4(){
+  var resposta = document.getElementById("certa4");
+  
+  if(resposta.checked){
+    alert("Acertou. Parabéns!");
+  }
+  else{
+    alert("Errou. que pena!");
+  }
+
+}
+
+function verificar5(){
+  var resposta = document.getElementById("certa5");
+  
+  if(resposta.checked){
+    alert("Acertou. Parabéns!");
+  }
+  else{
+    alert("Errou. que pena!");
+  }
+
+}
+
+function agradecer(){
+  alert("Obrigado por participar do nosso quiz!");
+}
